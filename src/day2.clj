cb932069898cded90d6217658184b388db5a6ac0
Adam Jeniski <adam.jeniski@flashtalking.com>
2023-10-04 12:26:30 -0400

    use reduce over loop

--- src/day2.clj
+++ src/day2.clj
@@ -5,30 +5,23 @@
 
 (def puzzle-input (get-puzzle-input 2))
 
-(defn count-letters [s]
-  (->> (frequencies s)
-       (vals)
-       (frequencies)))
-
 ;; part 1
-(let [counts
-      (reduce (fn [acc curr]
-                (let [counts (count-letters curr)]
-                  (-> acc
-                      (update :two   #(if (counts 2) (inc %) %))
-                      (update :three #(if (counts 3) (inc %) %)))))
-              {:two 0, :three 0}
-              puzzle-input)]
-  (* (:two counts)
-     (:three counts)))
+(->> puzzle-input
+     (reduce (fn [acc word]
+               (let [counts (-> word frequencies vals frequencies)]
+                 (-> acc
+                     (update :two   #(if (counts 2) (inc %) %))
+                     (update :three #(if (counts 3) (inc %) %)))))
+             {:two 0, :three 0})
+     vals
+     (apply *))
 
 ;; part 2
-(loop [[word & rest-words] puzzle-input
-       seen #{}]
-  (let [substrs (->> (range (count word))
-                     (map #(str (subs word 0 %) (subs word (inc %))))
-                     (into #{}))]
-    (or (some seen substrs)
-        (recur rest-words
-               (union seen substrs)))))
-
+(reduce (fn [seen word]
+          (let [substrs (->> (range (count word))
+                             (map #(str (subs word 0 %) (subs word (inc %))))
+                             (into #{}))]
+            (or (some-> (some seen substrs) reduced)
+                (union seen substrs))))
+        #{}
+        puzzle-input)
