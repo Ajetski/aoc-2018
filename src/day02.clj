a85beb4a5c12eee420eadcabc273ea8cd76822ff
Adam <ajeniski4@gmail.com>
2023-10-06 13:22:24 -0400

    bc why not?

--- src/day02.clj
+++ src/day02.clj
@@ -1,7 +1,7 @@
 (ns ^{:doc "Day 2: Inventory Management System"
       :author "Adam Jeniski"}
  day02 (:require [core :refer [get-puzzle-input]]
-                [clojure.set :refer [union]]))
+                 [clojure.set :refer [union]]))
 
 (def words (get-puzzle-input 2))
 
@@ -25,3 +25,19 @@
                 (union seen substrs))))
         #{}
         words)
+
+;; part 1, but more generic bc why not?
+(->> words
+     (reduce (fn [acc word]
+               (let [counts (-> word frequencies vals frequencies)]
+                 (reduce (fn [acc num]
+                           (if (counts num)
+                             (update acc num inc)
+                             acc))
+                         acc
+                         (keys acc))))
+             (->> [2 3]
+                  (map #(vector % 0))
+                  (into {})))
+     vals
+     (reduce *))
